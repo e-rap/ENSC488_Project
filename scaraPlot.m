70217bafd56699173f138f42f309f519e7a6b5e1
Clara Tsang <clarat@sfu.ca>
2016-12-03 11:28:45 -0800

    Updated scaraPlot
    initial commit for plotting for final demo

--- scaraPlot.m
+++ scaraPlot.m
@@ -1,18 +1,197 @@
-fileID = fopen('points.txt', 'r');
-formatSpec = '%f %f %f ';
-sizeA = [3 1200];
-A = fscanf(fileID, formatSpec, sizeA);
-fclose(fileID);
 
-A = A';
+% Opening files
 
-[m,n] = size(A);
+plannedPFile = fopen('plannedP.txt', 'r');
+plannedVFile = fopen('plannedV.txt', 'r');
+plannedAFile = fopen('plannedA.txt', 'r');
 
-x = A(1:m, 1);
-y = A(1:m, 2);
-z = A(1:m, 3);
+simPFile = fopen('simP.txt', 'r');
+simVFile = fopen('simV.txt', 'r');
+simAFile = fopen('simA.txt', 'r');
 
-figure 
-plot3(x, y, z)
+XYFile = fopen('XY.txt', 'r');
+
+% Setting format to read data (input file should be 'theta1 theta2 d3 theta4 /n')
+
+formatSpec1 = '%f %f %f '; % for joint variable files
+formatSpec2 = '%f %f '; % for x-y mapping file
+
+% Set up size of array where we'll be putting data
+
+size1 = [4 Inf];
+size2 = [2 Inf];
+
+% Putting file contents into array of column vectors
+
+PP = fscanf(plannedPFile, formatSpec1, size1);
+PV = fscanf(plannedVFile, formatSpec1, size1);
+PA = fscanf(plannedAFile, formatSpec1, size1);
+
+SP = fscanf(simPFile, formatSpec1, size1);
+SV = fscanf(simVFile, formatSpec1, size1);
+SA = fscanf(simAFile, formatSpec1, size1);
+
+XY = fscanf(XYFile, formatSpec2, size2);
+
+% Closing files
+
+fclose(plannedPFile);
+fclose(plannedVFile);
+fclose(plannedAFile);
+
+fclose(simPFile);
+fclose(simVFile);
+fclose(simAFile);
+
+fclose(XYFile);
+
+% Extracting info from files for plottable variables
+
+[m,n] = size(PP);
+[a,b] = size(SP);
+[c,d] = size(XY);
+
+% Planned position
+
+theta1PP = PP(1, 1:n);
+theta2PP = PP(2, 1:n);
+d3PP = PP(3, 1:n);
+theta4PP = PP(4, 1:n);
+
+% Planned velocity
+
+theta1PV = PV(1, 1:n);
+theta2PV = PV(2, 1:n);
+d3PV = PV(3, 1:n);
+theta4PV = PV(4, 1:n);
+
+% Planned accel
+
+theta1PA = PA(1, 1:n);
+theta2PA = PA(2, 1:n);
+d3PA = PA(3, 1:n);
+theta4PA = PA(4, 1:n);
+
+% Simulator Output position
+
+theta1SP = SP(1, 1:b);
+theta2SP = SP(2, 1:b);
+d3SP = SP(3, 1:b);
+theta4SP = SP(4, 1:b);
+
+% Simulator Output velocity
+
+theta1SV = SV(1, 1:b);
+theta2SV = SV(2, 1:b);
+d3SV = SV(3, 1:b);
+theta4SV = SV(4, 1:b);
+
+% Simulator Output accel
+
+theta1SA = SA(1, 1:b);
+theta2SA = SA(2, 1:b);
+d3SA = SA(3, 1:b);
+theta4SA = SA(4, 1:b);
+
+% X-Y Mapping
+
+x = XY(1,1:d);
+y = XY(2,1:d);
+
+
+% PLOTTING!!!!!!! -----------------------------------
+
+
+
+tPlanned = 0:0.01:10; % depends on sample time
+tSim = 0:0.001:10; % depends on time T3
+
+figure
+
+% Joint Positions
+
+subplot(3,4,1)
+plot(tPlanned, theta1PP, tSim, theta1SP)
+title('Joint 1 Position')
+xlabel('Time')
+ylabel('Position (\circ)')
+
+subplot(3,4,2)
+plot(tPlanned, theta2PP, tSim, theta2SP)
+title('Joint 2 Position')
+xlabel('Time')
+ylabel('Position (\circ))')
+
+subplot(3,4,3)
+plot(tPlanned, d3PP, tSim, d3SP)
+title('Joint 3 Position')
+xlabel('Time')
+ylabel('Position (mm)')
+
+subplot(3,4,4)
+plot(tPlanned, theta4PP, tSim, theta4SP)
+title('Joint 4 Position')
+xlabel('Time')
+ylabel('Position (\circ))')
+
+% Joint Velocities
+
+subplot(3,4,5)
+plot(tPlanned, theta1PV, tSim, theta1SV)
+title('Joint 1 Velocity')
+xlabel('Time')
+ylabel('Velocity (\circ)/s)')
+
+subplot(3,4,6)
+plot(tPlanned, theta2PV, tSim, theta2SV)
+title('Joint 2 Velocity')
+xlabel('Time')
+ylabel('Velocity (\circ)/s)')
+
+subplot(3,4,7)
+plot(tPlanned, d3PV, tSim, d3SV)
+title('Joint 3 Velocity')
+xlabel('Time')
+ylabel('Velocity (mm/s)')
+
+subplot(3,4,8)
+plot(tPlanned, theta4PV, tSim, theta4SV)
+title('Joint 4 Velocity')
+xlabel('Time')
+ylabel('Velocity (\circ)/s)')
+
+% Joint Accelerations
+
+subplot(3,4,9)
+plot(tPlanned, theta1PA, tSim, theta1SA)
+title('Joint 1 Acceleration')
+xlabel('Time')
+ylabel('Acceleration (\circ/s^{2})')
+
+subplot(3,4,10)
+plot(tPlanned, theta2PA, tSim, theta2SA)
+title('Joint 2 Acceleration')
+xlabel('Time')
+ylabel('Acceleration (\circ/s^{2})')
+
+subplot(3,4,11)
+plot(tPlanned, d3PA, tSim, d3SA)
+title('Joint 3 Acceleration')
+xlabel('Time')
+ylabel('Acceleration (mm/s^{2})')
+
+subplot(3,4,12)
+plot(tPlanned, theta4PA, tSim, theta4SA)
+title('Joint 4 Acceleration')
+xlabel('Time')
+ylabel('Acceleration (\circ/s^{2})')
+
+% X-Y Map
+
+figure
+plot(x,y)
+title('X-Y Path of Tool')
+xlabel('X')
+ylabel('Y')
 
 
